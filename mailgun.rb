3c2114ab84c6391013201cde81cc5d23619a13a2
mangala <mangala.k@theamp.com>
2017-02-24 12:52:47 +0530

    detect if an email address is listed in a suppressions list

--- mailgun.rb
+++ mailgun.rb
@@ -2,6 +2,8 @@ require 'rest-client' #gem install rest-client, gem install byebug, #use ruby ve
 require 'pry'
 require 'json'
 require 'yaml'
+require 'thread'  # for Mutex
+
 
 $cnf = YAML::load_file(File.join(__dir__, 'secrets.yml'))
 
@@ -21,24 +23,52 @@ module Mailgun
   
   end
 
-# def detect_suppression_email
+  def add_bounce
+    begin
+      RestClient.post $cnf['mailgun']['apikey'] + $cnf['mailgun']['bounnces'],
+                    :address => 'bkotu6717@gmail.com'
+    rescue Exception => e
+        puts "Exception raised add_bounce:" + e.class.to_s
+        puts e.message
+    end
+  end
 
-# end
+  def self.detect_suppression_email(email_id)
+    begin
+    suppression_email_list = []
+    urls = [
+    {'link' => $cnf['mailgun']['apikey'] + $cnf['mailgun']['bounnces'] + "/#{email_id}"},
+    {'link' => $cnf['mailgun']['apikey'] + $cnf['mailgun']['unsubscribes'] + "/#{email_id}"},
+    {'link' => $cnf['mailgun']['apikey'] + $cnf['mailgun']['complaints'] + "/#{email_id}"}
+  ]
 
-# def get_bounces
-#   RestClient.get "https://api:YOUR_API_KEY"\
-#   "@api.mailgun.net/v3/YOUR_DOMAIN_NAME/bounces"
-# end
-
-# def get_unsubscribes
-#   RestClient.get "https://api:YOUR_API_KEY"\
-#   "@api.mailgun.net/v3/YOUR_DOMAIN_NAME/unsubscribes"
-# end
-# def get_complaints
-#   RestClient.get "https://api:YOUR_API_KEY"\
-#   "@api.mailgun.net/v3/YOUR_DOMAIN_NAME/complaints"
-# end
-
+    urls.each do |u|
+      Thread.new do
+        u['content'] = RestClient.get(u['link']){|response, request, result| response }
+        puts "Successfully requested #{u['link']}"
+        data = JSON.parse(response.body)
+        (suppression_email_list << email_id) if response.code == 200
+        if urls.all? {|u| u.has_key?("content") }
+          puts "Fetched all urls!"
+          exit
+        end
+      end
+      sleep 3
+    end
+     # Check email address is listed in a suppressions list
+      # if suppression_email_list.present?
+      #   puts "#{email_id} address found in suppression list"
+        # return true
+      # else
+      #   puts "#{email_id} Address not found in suppression list"
+        # return false
+      # end
+    rescue Exception => e
+      puts "Exception raised send_mail:" + e.class.to_s
+      puts e.message
+    end
+    suppression_email_list
+  end
 
   def self.previously_sent_emails  #looks like this is not correct, should check
   	previously_sent_emails_list = []
--- mailgun_spec.rb
+++ mailgun_spec.rb
@@ -11,4 +11,18 @@ describe Mailgun do
   		expect(response['message']).to eq('Queued. Thank you.')
 	  end
   end 
+
+   describe "#detect_suppression_email" do
+    it "detect suppression email" do
+      response = Mailgun.detect_suppression_email('bkotu6717@gmail.com')
+      binding.pry
+      expect(suppression_email_list).not_to nil
+
+      # response = JSON.parse(response.body)
+      # expect(response).to have_key('id')
+      # expect(response).to have_key('message')
+      # expect(response['message']).to eq('Queued. Thank you.')
+    end
+  end 
+
 end
